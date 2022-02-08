d3510971b9c65ebeb4f98a7b0ff4f53ab34b2cde
aziz gadacha <aziz.gadacha.2000@gmail.com>
2022-02-08 23:52:43 +0100

    changement1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Todo from "./components/todo/Todo";
 function App() {
   return (
       <div>
+
+        <h1>Hello Gays</h1>
     <Todo/>
       </div>
   );
--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -2,8 +2,13 @@
 function Todo() {
     return (
 
-
-        <h1> hello baby</h1>
+<div>
+        <ul className="list-group">
+            <li className="list-group-item"><input className="form-check-input me-1" type="checkbox" />{6+18}Facebook</li>
+            <li className="list-group-item"><input className="form-check-input me-1" type="checkbox" />Twitter</li>
+            <li className="list-group-item"><input className="form-check-input me-1" type="checkbox" />Instagrame</li>
+        </ul>
+</div>
     );
 }
 
