84d9c43a8c5de4e4dcf7ad317555490df2e93f50
aziz gadacha <aziz.gadacha.2000@gmail.com>
2022-02-09 01:02:06 +0100

    changement3

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -4,9 +4,9 @@ function Todo() {
 
 <div>
         <ul className="list-group">
-            <li className="list-group-item"><input className="form-check-input me-1" type="checkbox" />{6+18}Facebook</li>
-            <li className="list-group-item"><input className="form-check-input me-1" type="checkbox" />Twitter</li>
-            <li className="list-group-item"><input className="form-check-input me-1" type="checkbox" />Instagrame</li>
+            <li className="list-group-item"><input className="form-check-input me-1" id="facebook" type="checkbox" />{6+18} <label >Facebook</label></li>
+            <li className="list-group-item"><input className="form-check-input me-1" id="twitter" type="checkbox" />  <label >Twitter</label> </li>
+            <li className="list-group-item"><input className="form-check-input me-1" id="instagrame" type="checkbox" /><label >Instagrame</label></li>
         </ul>
 </div>
     );
